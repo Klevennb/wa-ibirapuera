519c8d14385e068c5e3a348ba9b50168e29d859d
Matthew Kleven <matthew.kleven@traivefinance.com>
2025-02-14 16:25:40 -0600

    toolbar skelington

--- src/components/editor/editor-wrapper.tsx
+++ src/components/editor/editor-wrapper.tsx
@@ -8,6 +8,7 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { EditorState } from "lexical";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import ToolbarPlugin from "./plugins/toolbar";
 // import { EditorTheme } from "./theme"; // Optional for further styling
 
 // Handle editor state changes
@@ -29,7 +30,7 @@ const AutoFocusPlugin = () => {
 // Editor configuration
 const editorConfig = {
   namespace: "MyEditor",
-//   theme: EditorTheme,
+  //   theme: EditorTheme,
   onError(error: Error) {
     console.error(error);
   },
@@ -41,12 +42,18 @@ export default function LexicalEditor() {
     <LexicalComposer initialConfig={editorConfig}>
       <div className="relative w-full max-w-2xl mx-auto bg-white border border-gray-300 shadow-lg rounded-xl p-4">
         {/* Editor Content */}
+        <ToolbarPlugin />
+
         <div className="relative min-h-[250px] p-4 bg-gray-50 border border-gray-200 rounded-lg">
           <RichTextPlugin
             contentEditable={
               <ContentEditable className="w-full min-h-[200px] outline-none bg-transparent text-gray-800 text-base leading-relaxed" />
             }
-            placeholder={<div className="absolute top-4 left-4 text-gray-400">Start typing...</div>}
+            placeholder={
+              <div className="absolute top-4 left-4 text-gray-400">
+                Start typing...
+              </div>
+            }
             ErrorBoundary={(error) => <div>Error: {error.children}</div>}
           />
           <OnChangePlugin onChange={onChange} />
---
+++ src/components/editor/plugins/toolbar/index.tsx
@@ -0,0 +1,35 @@
+import toolbarIconsList from "./toolbar-icon-list";
+import useOnClickListener from "./toolbar-onclick-listener";
+
+
+const LexicalEditorTopBar = () => {
+  const { onClick, selectedEventTypes, blockType } =
+    useOnClickListener();
+
+  const isIconSelected = (plugin: any) =>
+    selectedEventTypes.includes(plugin.event) ||
+    blockType.includes(plugin.event);
+
+  return (
+    <div className="container justify-between  flex space-x-2 p-1"
+
+    >
+      {toolbarIconsList.map((plugin) => (
+        <div 
+          key={plugin.id}
+          className="cursor-pointer"
+        
+        >
+          {
+            <plugin.Icon
+              onClick={() => onClick(plugin.event)}
+              color={isIconSelected(plugin) ? "secondary" : undefined}
+            />
+          }
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LexicalEditorTopBar;
