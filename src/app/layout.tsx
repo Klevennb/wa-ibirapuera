be6094b17d57da52044a39588d8605b8aefd4f8b
Matthew Kleven <matthew.kleven@traivefinance.com>
2025-03-11 16:57:59 -0500

    added sidebar wrapper and strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { EditorProvider } from "@/contexts/editor-context";
+import SidebarWrapper from "@/components/sidebar/sidebar-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,17 +24,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <EditorProvider>
-                {children}
-      </EditorProvider>
+        <EditorProvider>
+          <SidebarWrapper>{children}</SidebarWrapper>
+        </EditorProvider>
       </body>
     </html>
   );
 }
-  
