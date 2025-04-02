5df12c81664a19f3496a43fb1da6f2e23ea3d6af
Matthew Kleven <klevennb115dev@gmail.com>
2025-04-01 23:24:02 -0500

    temporarily removed register actions

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,21 @@
 "use server"
 
 import { RegisterForm } from "@/components/register/register-form"
-import { FormEvent } from "react"
 
 export default function RegisterPage() {
-    const handleSubmit = (data: FormEvent<HTMLFormElement>) => {
-        data.preventDefault()
-        console.log('Form Data: ', data)
-    }
+    // const handleSubmit = (data: FormEvent<HTMLFormElement>) => {
+    //     data.preventDefault()
+    //     console.log('Form Data: ', data)
+    // }
 
-    const registerWithGoogle = () => {
-        return
-    }
+    // const async registerWithGoogle = () => {
+    //     return
+    // }
 
 
     return (
         <>
-            <RegisterForm handleGoogleRegister={registerWithGoogle} handleSubmit={handleSubmit} />
+            <RegisterForm />
         </>
     )
 }
