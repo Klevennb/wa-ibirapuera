98289c1e59e892bb767eb1ef296efd882c7dfb4d
Matthew Kleven <matthew.kleven@traivefinance.com>
2025-04-09 15:29:55 -0500

    removed unused import

--- src/components/register/register-form.tsx
+++ src/components/register/register-form.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-  import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { FormEvent, useRef, useState } from "react";
+import { FormEvent, useState } from "react";
 import { signUp } from "@/app/actions/auth/auth";
 import { SignUpFields } from "@/types/auth";
 
